220e3b4f1c6515a226e0a7cbf831563edf62ba4a
Natsuki Hasegawa <oosacker@gmail.com>
2020-08-04 15:09:42 +1200

    edit the template as list

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,20 @@ Route::get('/', function () {
     // return view('welcome');
     $links = \App\Link::all();
     return view('welcome', ['links' => $links]);
+});
+
+Route::get('/submit', function () {
+    return view('submit');
+});
+
+Route::post('/submit', function (Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'url' => 'required|url|max:255',
+        'description' => 'required|max:255',
+    ]);
+    $link = tap(new App\Link($data))->save();
+    return redirect('/');
 });
 
 Route::redirect('/go', 'https://google.com');
